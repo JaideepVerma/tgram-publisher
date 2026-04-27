5eff384dfa731e57dc9dddd5073fef3cb3e3932f
JaideepVerma <61654942+JaideepVerma@users.noreply.github.com>
2026-04-27 11:35:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,9 @@ def process_jobs():
     for job in jobs:
         job_id = job["job_id"]  # must be unique per job
         job_company =job["company"]
+        print(job_company)
         print(job_company+ ' ' + job_id not in sent_ids , job["posting_date"] == str(todaysDate) )
+        
         if job_company+ ' ' + job_id not in sent_ids and job["posting_date"] == str(todaysDate): #"23-04-2026": 
             print('Loading...')
             send_job(job["company"], job["role"], job["location"], job["apply_link"],job["posting_date"])
